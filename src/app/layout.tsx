9e44f10af7ac927485cf5bd5d535b76f57c25d8a
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2024-09-27 01:33:43 -0300

    chore: trigger build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Cefas Garcia Pereira",
   description:
     "Cefas Garcia Pereira has Master in Software Engineering (PUC-MG) and Bachelor in Computer Science (PUC-MG).",
-  keywords: "cefas, software engineer, developer, fullstack, reactjs, master, software, code, machine learning, front end, fullstack",
+  keywords: "cefas, software engineer, developer, fullstack, reactjs, master, software, code, machine learning, fullstack, front end",
 };
 
 export default function RootLayout({
